016676caad550766df53ca632043b20d9f5c5629
Zack Stout <zackstout@gmail.com>
2018-07-27 00:23:35 -0500

    changing

--- routes/poems.router.js
+++ routes/poems.router.js
@@ -15,7 +15,7 @@ if (process.env.DATABASE_URL) {
         host: params.hostname,
         port: params.port,
         user: 'zackstout@gmail.com',
-        password: 'Encounter1!',
+        password: 'encounter',
         database: params.pathname.split('/')[1],
         ssl: true, // heroku requires ssl to be true
         max: 10, // max number of clients in the pool
